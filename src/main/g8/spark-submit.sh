ae3d0bb7a899999f25aea497bcde97f0e70c680c
wopoadi <133659712+wopoadi@users.noreply.github.com>
2024-12-19 13:17:46 +0100

    Delete src/main/g8/spark-submit.sh

--- src/main/g8/spark-submit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-/opt/bitnami/spark/bin/spark-submit \
-    --deploy-mode "client" \
-    --master "spark://spark-master:7077" \
-    --executor-cores 4 \
-    --executor-memory 2G \
-    --num-executors 1 \
-    --class "$class_name$" \
-    "target/scala-2.12/$name;format="normalize"$_2.12-$app_version$.jar" \
