7817df15d7ae90712a4e6e6c459f7d56918f85bc
wopoadi <kodjo@osekoo.com>
2024-12-19 16:33:32 +0100

    Fix $ escaping error

--- src/main/g8/spark-submit.sh
+++ src/main/g8/spark-submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 spark-submit \
     --deploy-mode client \
-    --master "$SPARK_MASTER_URL" \
+    --master "\$SPARK_MASTER_URL" \
     --executor-cores 4 \
     --executor-memory 2G \
     --num-executors 1 \
