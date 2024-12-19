0f740b8849c952728058ae6bd0881ec3647ae5ea
wopoadi <133659712+wopoadi@users.noreply.github.com>
2024-12-19 13:18:35 +0100

    Update run-app.bat

--- src/main/g8/run-app.bat
+++ src/main/g8/run-app.bat
@@ -1,3 +1,14 @@
 @echo off
+echo Stopping Spark cluster...
+docker rm -f spark-master
+docker rm -f spark-worker
+
+echo Starting Spark cluster...
 docker-compose up -d
-docker exec -it spark-master /app/spark-submit.sh
+
+echo Waiting for Spark master to be ready...
+timeout 10
+
+echo Submitting Spark job...
+docker exec -it spark-master ./spark-submit.sh
+echo Spark job submitted.
