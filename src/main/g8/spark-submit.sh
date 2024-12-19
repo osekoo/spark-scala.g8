56c81869f84c96ad7b89511c2cae9d02850c4fbd
wopoadi <133659712+wopoadi@users.noreply.github.com>
2024-12-19 13:32:33 +0100

    Update spark-submit.sh

--- src/main/g8/spark-submit.sh
+++ src/main/g8/spark-submit.sh
@@ -5,5 +5,5 @@
     --executor-cores 4 \
     --executor-memory 2G \
     --num-executors 1 \
-    --class "$class_name$" \
+    --class "$class_name;format="Camel"$" \
     "target/scala-2.12/$name;format="normalize"$_2.12-$app_version$.jar" \
