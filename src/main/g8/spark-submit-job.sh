1be8f1adc476a888e3d4334852b995dc8b292741
wopoadi <kodjo@osekoo.com>
2025-01-16 13:01:28 +0100

    Update execution scripts

--- src/main/g8/spark-submit-job.sh
+++ src/main/g8/spark-submit-job.sh
@@ -2,9 +2,9 @@
 spark-submit \
     --deploy-mode client \
     --master "spark://localhost:7077" \
-    --executor-cores 4 \
+    --executor-cores 1 \
     --executor-memory 2G \
-    --num-executors 1 \
+    --num-executors 8 \
     --class "$class_name;format="Camel"$" \
     "target/scala-2.12/$name;format="normalize"$_2.12-$app_version$.jar" \
-    100000000 \
+    1000000 \
