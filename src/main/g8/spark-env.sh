e267ab77ce1e865542c422bc22973146997849ad
wopoadi <kodjo@osekoo.com>
2025-01-16 16:14:04 +0100

    Update execution scripts

--- src/main/g8/spark-env.sh
+++ src/main/g8/spark-env.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker rm -f spark-env
 
-docker run -it --name spark-env -v "\$(pwd)":/workspace -h localhost -p 4040:4040 -p 8080:8080 -p 8081:8081 -e SPARK_MODE=env ghcr.io/osekoo/spark:3.5
+docker run -it --rm --name spark-env -v "\$(pwd)":/workspace -h localhost -p 4040:4040 -p 8080:8080 -p 8081:8081 -e SPARK_MODE=env ghcr.io/osekoo/spark:3.5
